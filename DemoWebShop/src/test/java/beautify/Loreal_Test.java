ddb474df8cf2b8dfa440a6dee2561a5f6ab0b597
Geetha <Geetha@DESKTOP-B84KNI1>
2024-03-29 19:28:20 +0530

    I have added a test Script inside beauty package

---
+++ DemoWebShop/src/test/java/beautify/Loreal_Test.java
@@ -0,0 +1,18 @@
+package beautify;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Loreal_Test {
+
+	@Test
+	public void loreal() {
+
+		System.out.println("Hello");
+
+	}
+	
+}
